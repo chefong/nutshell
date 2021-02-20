e5485d5db1868c2078027243894369c40603bbf5
chefong <ericong18@gmail.com>
2021-02-20 13:12:14 -0800

    Add info modal to video page

--- frontend/src/pages/Video/Video.js
+++ frontend/src/pages/Video/Video.js
@@ -6,6 +6,7 @@ import logo from '../../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
 import closeIcon from '../../assets/images/x.svg';
 import blackThumbnail from '../../assets/images/black-thumbnail.png';
+import { Modal } from 'rsuite';
 
 const mockTranscriptInfo = {
   videoTitle: 'CSS Outline vs. Border',
@@ -66,16 +67,26 @@ const mockKeypointsInfo = [
 
 function Video() {
   const [showBanner, setShowBanner] = useState(true);
+  const [showInfoModal, setShowInfoModal] = useState(false);
 
   const handleBannerClose = (e) => {
     e.preventDefault();
     setShowBanner(false);
   };
 
+  const handleLearnClick = (e) => {
+    e.preventDefault();
+    setShowInfoModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowInfoModal(false);
+  };
+
   const renderInfoBanner = () => {
     return (
       <div className="Video__banner">
-        <p className="Video__banner-text">Wow! That was fast, wasn’t it?  <span className="Video__banner-link">Click here to find out why</span></p>
+        <p className="Video__banner-text">Wow! That was fast, wasn’t it?  <span className="Video__banner-link" onClick={handleLearnClick}>Click here to find out why</span></p>
         <img src={closeIcon} className="Video__banner-close" onClick={handleBannerClose} alt=""/>
       </div>
     );
@@ -121,6 +132,21 @@ function Video() {
           ))}
         </div>
       </div>
+      <Modal size='sm' show={showInfoModal} onHide={handleModalClose} className="Video__info-modal">
+        <Modal.Header className="Video__info-modal-header">
+          <Modal.Title>Previously Processed Videos</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>As you may have previously realized, the video conversion process takes a really long time due to all the things happening behind the scenes. Fortunately for you, any video that was previously processed (by you or some other user) will get cached in some remote store. That simply means less waiting and more consuming!</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="Video__info-modal-footer">
+            <Button onClick={handleModalClose} appearance="primary">
+              Ok
+            </Button>
+          </div>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
