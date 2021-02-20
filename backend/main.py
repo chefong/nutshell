a29a66283405ae40b50750dc018f359d89461d66
ckong007 <ckong007@ucr.edu>
2021-02-20 13:31:53 -0800

    downloads the vid on post request

--- backend/main.py
+++ backend/main.py
@@ -14,28 +14,20 @@ def welcome():
 def ibmz():
     return "<p style=\"font-size:100px\">&#10024; IBM-Z IS GREAT &#10024;</p>"
 
-@app.route('/download')
-def download():
-    yt_url = input('Input YouTube URL: ')
-    print("Downloading...")
-    video = YouTube(yt_url)
-    video.streams[0].download(VID_DIR)
-    print("Download completed!!")
-    print("Length of video: ", video.length, "seconds")
-    # uh gotta have a way to check if a video is already downloaded?
-    # get a way to change mp4 to wav or flac
-
 @app.route('/api/submit', methods = ['POST'])
 def submit():
     print(request.json['url'])
     if request.json is None:
         return 'ERROR: no JSON in request body'
     yt_url = request.json['url']
+    video = YouTube(yt_url)
     if not yt_url.startswith('https://youtube.com/') and not yt_url.startswith('https://www.youtube.com/'):
         return { 'success': False, 'message': 'Invalid URL' }
 
     vid_id = extract.video_id(yt_url)
-    filename = path.join(VID_DIR, f'{vid_id}.mp4')
+    filename = f'{vid_id}'
+    video.streams[0].download(VID_DIR, filename)
+    print("Length of video: ", video.length, "seconds")
     return filename
 
 '''
